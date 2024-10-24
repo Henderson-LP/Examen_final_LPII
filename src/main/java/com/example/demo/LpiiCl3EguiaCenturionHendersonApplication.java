bb3f463c8f404be02cf6695401df9dc787c6a854
Henderson-LP <hendersontupro24@gmail.com>
2024-10-23 20:35:24 -0500

    Primer commit - Creacion de Proyecto

---
+++ src/main/java/com/example/demo/LpiiCl3EguiaCenturionHendersonApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiCl3EguiaCenturionHendersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiCl3EguiaCenturionHendersonApplication.class, args);
+	}
+
+}
